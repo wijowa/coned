da009ed981a536b82c389755223011dd2c63070a
unknown <Williqm@.(none)>
2012-05-20 02:45:23 -0400

    Added supply charges to ConedBill.  Supply charges require loading and parsing a new page for each row in the billing history to grab a single price number.

--- coned.py
+++ coned.py
@@ -14,7 +14,7 @@ ConedBill is a wrapper class that pulls account energy usage history from an acc
     cb.csv
     
 @todo: 
-    It's possible to grab supply charges from Coned's site through a separate ASP service.
+    Provide an ESPI wrapper to export in ESPI standard XML format directly
 
 @copyright: William Wagner / Econofy 2012
 @author: William Wagner
@@ -27,31 +27,80 @@ import urllib
 import urllib2
 import re
 import datetime
+import time
 import json
+import uuid
+from xml.dom.minidom import Document
 
 
 class ConedBill(object):
-    url='https://apps1.coned.com/csol/billhist.asp'
+    history_url = 'https://apps1.coned.com/csol/billhist.asp'
+    msc_status_url = 'https://apps1.coned.com/csol/MSC.asp'
+    msc_url = 'https://apps1.coned.com/CSOL/MSC_Process.asp' 
+    
+
     rows=[]
     address=None
     account=None
     rownames = ['StartDate', 'EndDate', 'ElectricEnergyUsage','ElectricPowerDemand','MonthlyElectricBill','MonthlyGasUsage','MonthlyGasBill','TotalEnergyBill']
+    msc_values={}
+    d=None
     
+    @property
+    def urn(self):
+        return 'urn:uuid:%s' % uuid.uuid4()
+    @property
+    def now(self):
+        return datetime.datetime.now().isoformat()
+    
+    @property
+    def private_account(self):
+        return 'XXXXX%s'%self.account[-10:0]
+
     def __init__(self,account):
         self.account=account
         self.response=self._read_account()
         eb=self._get_account()
+        self._read_msc()
         
     def _read_account(self):
-
         values={'ACCT':self.account,'GOOD':0}
         data = urllib.urlencode(values)
-        req = urllib2.Request(self.url,data)
+        req = urllib2.Request(self.history_url,data)
+
         response = urllib2.urlopen(req)
         r = response.read().split('CONTENT FIELD', 1)[1].split('END CONTENT',1)[0]
 
+#            <input TYPE="HIDDEN" NAME="SOURCE" VALUE="MSC">
+#            <input TYPE="HIDDEN" NAME="LOADZONE", VALUE="J">
+#            <input TYPE="HIDDEN" NAME="SRVCLASS", VALUE="001,">
+#            <input TYPE="HIDDEN" NAME="EPRES", VALUE="100">
+#            <input TYPE="HIDDEN" NAME="FROMDATE">
+#            <input TYPE="HIDDEN" NAME="TODATE">
         return r
+    
+    def _read_msc(self):
+        values = {'ACCT':self.account,'GOOD':0}
+        data = urllib.urlencode(values)
+        
+        req = urllib2.Request(self.msc_status_url,data)
 
+        response = urllib2.urlopen(req)
+        r = response.read().split('CONTENT FIELD', 1)[1].split('END CONTENT',1)[0]
+        
+        self.msc_values={'ACCT':values['ACCT'],'GOOD':0,'SOURCE':'MSC','LOADZONE':'J','SRVCLASS':'001','EPRES':'100'}
+        #print r.split('NAME="SOURCE" VALUE="',1)[1].split('">',1)[0]
+
+        #print r.split('NAME="LOADZONE" VALUE="',1)[1].split('">',1)[0]
+        self.msc_values['SOURCE']=r.split('NAME="SOURCE" VALUE="',1)[1].split('">',1)[0]
+        self.msc_values['LOADZONE']=r.split('NAME="LOADZONE", VALUE="',1)[1].split('">',1)[0]
+        self.msc_values['SRVCLASS']=r.split('NAME="SRVCLASS", VALUE="',1)[1].split('">',1)[0]
+        self.msc_values['EPRES']=r.split('NAME="EPRES", VALUE="',1)[1].split('">',1)[0]
+        
+        
+        self.type = r.split('<td align="left">Electric Rate:</td>',1)[1].split('<td align="left">',1)[1].split('</td>',1)[0].strip()
+            
+        
     def _get_account(self):
         r=self.response
         rowvalue='<td[\w\W]+?><font[\w\W]+?>([\w\W]+?)<\/font>'
@@ -74,35 +123,73 @@ class ConedBill(object):
         self.rows=rows
         self.address=address
         return self
+    
+    def _get_supply_charge(self,datefrom,dateto):
+        
+        values=self.msc_values
+        values['FROMDATE']=datefrom
+        values['TODATE']=dateto
+             
 
+
+        data = urllib.urlencode(values)
+        req = urllib2.Request(self.msc_url,data)
+
+        response = urllib2.urlopen(req)
+        r = response.read().split('CONTENT FIELD', 1)[1].split('END CONTENT',1)[0]
+        
+        return r.split('<b>Supply Charge</b>',1)[1].split('SIZE="2">',1)[1].split('</font>',1)[0]
+        
+    
+    
     @property
     def dict(self):
         eb = self
-        d={'Account':eb.account,'Address':eb.address,'History':[]}
-        for r in eb.rows:
-            try:
-                sd = r[0].split('/')
-                ed = r[1].split('/')
-                startdate = datetime.date(month=int(sd[0]),day=int(sd[1]),year=int(sd[2]))
-                enddate = datetime.date(month=int(ed[0]),day=int(ed[1]),year=int(ed[2]))
-                days=(enddate-startdate).days             
+        if(eb.d == None):
+            d={'Account':eb.account,'Address':eb.address,'Type':eb.type,'History':[],'Start':0,'Duration':0}
+            for r in eb.rows:
                 try:
-                    kwh=round(float(''.join(r[2].split(','))))
-                    daily=round(kwh/float(days))
-                    power=round(kwh/float(days*24.0)*1000.0)
-                except ValueError:
-                    #data not available in this interval
-                    kwh=None
-                    daily=None
-                    power=None
-
-                d['History'].append({'StartDate':r[0],'EndDate':r[1],'Duration':days,'kWh':kwh,'USD':r[7],
-                                     'Daily':daily,'Power':power})
-                
-            except IndexError:
-                pass
-        return d
+                    sd = r[0].split('/')
+                    ed = r[1].split('/')
+                    startdate = datetime.date(month=int(sd[0]),day=int(sd[1]),year=int(sd[2]))
+                    enddate = datetime.date(month=int(ed[0]),day=int(ed[1]),year=int(ed[2]))
+                    days=(enddate-startdate).days      
+                    seconds = days*24*3600
+                    start = int(time.mktime(startdate.timetuple()))
+                    
+                    supply = round(float(eb._get_supply_charge(r[0],r[1]))/100.0,4)
+                    
+                    try:
+                        kwh=round(float(''.join(r[2].split(','))))
+                        daily=round(kwh/float(days))
+                        power=round(kwh/float(days*24.0)*1000.0)
     
+                        delivery = round(float(r[7])/kwh,4)
+                        total_bill = round((supply+delivery)*kwh,2)
+                    except ValueError:
+                        #data not available in this interval
+                        kwh=None
+                        daily=None
+                        power=None
+                        delivery = None
+                        total_bill=None
+                    
+                    if (d['StartSeconds']==0):
+                        d['StartSeconds']=start
+                        
+                    d['TotalDurationSeconds']+=seconds
+                    
+                    d['History'].append({'StartDate':r[0],'EndDate':r[1],'Start':start,'Duration':seconds,'Usage':kwh,'DurationDays':days,
+                                         'Daily':daily,'Power':power,'SupplyCharge':supply,'DeliveryCharge':delivery,'TotalBill':total_bill})
+                    
+                except IndexError:
+                    pass
+                
+            eb.d=d
+            return d
+        else:
+            return eb.d
+        
     @property    
     def json(self):
         return json.dumps(self.dict)
@@ -116,4 +203,3 @@ class ConedBill(object):
         return s
     
             
-        
